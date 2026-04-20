69315e33bea31de5e111aa9b974d91d8c7c23a5b
Luiz Fernando <luisferndantas@outlook.com>
2026-04-19 22:26:28 -0300

    feat: adicionar configuração CORS para permitir requisições da aplicação

---
+++ config/cors.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'logout', 'register'],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => [
+        'http://coreva.luizfdev.com.br',
+        'https://coreva.luizfdev.com.br',
+    ],
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => true,
+
+];
